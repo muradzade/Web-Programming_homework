84002a544c5a346297d5bce1f8eb452d0fa912c0
Murad Zade <murad.abaszade@ogr.sakarya.edu.tr>
2019-12-24 16:32:44 +0200

    Kullsnici sayfalari

---
+++ Sportif/Pages/SalonDetails.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sportif.Data;
+
+namespace Sportif
+{
+    public class SalonDetailsModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public SalonDetailsModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Sportif.Models.Salon Salon { get; set; }
+        public List<Sportif.Models.Trainer> Trainers { get; set; }
+
+        public void OnGet(int id)
+        {
+            Salon = _db.Salons.FirstOrDefault(s => s.ID == id);
+            Trainers = _db.Trainers.Where(t => t.SalonID == id).ToList();
+        }
+    }
+}
---
+++ Sportif/Pages/Salons.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sportif.Data;
+
+namespace Sportif
+{
+    public class SalonsModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SalonsModel(ApplicationDbContext db)
+        {
+
+            _db = db;
+        }
+        public List<Sportif.Models.Salon> Salons { get; set; }
+        public List<Sportif.Models.Salon> SalonFiltered { get; set; }
+        public void OnGet(string adres = null)
+        {
+            Salons = _db.Salons.ToList();
+
+            if (adres == null)
+                SalonFiltered = _db.Salons.ToList();
+            else
+            {
+                SalonFiltered = _db.Salons.Where(s => s.Address == adres).ToList();
+
+            }
+        }
+
+
+
+    }
+}
